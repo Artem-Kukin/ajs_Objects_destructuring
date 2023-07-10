1554bead35a255574dc871638a3618e2c0364475
Artem <a.ku666@vk.com>
2023-07-10 18:06:39 +0200

    repair

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,9 @@ export default function specialAttackList(character) {
   const specialList = character.special;
   const result = [];
   specialList.forEach((element) => {
-    const {id, name, icon, description = 'Описание недоступно'} = element;
+    const {
+      id, name, icon, description = 'Описание недоступно'
+    } = element;
     result.push({
       id,
       name,
